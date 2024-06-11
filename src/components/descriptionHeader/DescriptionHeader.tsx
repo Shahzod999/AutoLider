4cfa957e20ed8b2e02bac64eab2556d2fc1876e5
Shahzod <sibragimov999@gmail.com>
2024-06-11 13:22:58 +0500

    arrows

--- src/components/descriptionHeader/DescriptionHeader.tsx
+++ src/components/descriptionHeader/DescriptionHeader.tsx
@@ -1,76 +1,85 @@
 import React from "react";
 import Image from "next/image";
 import "./descriptionHeader.scss";
+import { MdOutlineArrowForwardIos } from "react-icons/md";
 
 const DescriptionHeader = () => {
   return (
-    <div className="container">
-      <div className="descriptionHeader">
-        <div className="descBox">
-          <h2>Like a loan only easier</h2>
+    <>
+      <div className="container">
+        <div className="descriptionHeader">
+          <div className="descBox">
+            <h2>Like a loan only easier</h2>
 
-          <div className="descBox__calc">
-            <div className="descBox__calc__box">
-              <span>Car cost</span>
-              <span>50000000</span>
+            <div className="descBox__calc">
+              <div className="descBox__calc__box">
+                <span>Car cost</span>
+                <span>50000000</span>
+              </div>
+              <input type="range" id="initialFee" className="slider" min="100000" max="50000000" value="250000000" />
             </div>
-            <hr />
+
+            <div className="descBox__calc">
+              <div className="descBox__calc__box">
+                <span>An initial fee</span>
+                <span>5000000</span>
+              </div>
+              <input type="range" id="initialFee" className="slider" min="100000" max="5000000" value="2500000" />
+            </div>
+
+            <div className="descBox__calc">
+              <div className="descBox__calc__box">
+                <span>Term</span>
+                <span>12 months</span>
+              </div>
+              <input type="range" id="initialFee" className="slider" min="0" max="12" value="4" />
+            </div>
+
+            <div className="descBox__calc__payment">
+              <div>
+                <strong>50000</strong>
+                <span>UZS</span>
+                <span>/ Per day</span>
+              </div>
+
+              <div>
+                <strong>50000.00</strong>
+                <span>UZS</span>
+                <span>/ Per day</span>
+              </div>
+            </div>
           </div>
 
-          <div className="descBox__calc">
-            <div className="descBox__calc__box">
-              <span>An initial fee</span>
-              <span>5000000</span>
-            </div>
-            <hr />
-          </div>
-
-          <div className="descBox__calc">
-            <div className="descBox__calc__box">
-              <span>Term</span>
-              <span>12 months</span>
-            </div>
-            <hr />
-          </div>
-
-          <div className="descBox__calc__payment">
-            <div>
-              <strong>50000</strong>
-              <span>UZS</span>
-              <span>/ Per day</span>
-            </div>
-
-            <div>
-              <strong>50000.00</strong>
-              <span>UZS</span>
-              <span>/ Per day</span>
-            </div>
+          <div className="descBoxCar">
+            <Image src="/car1.png" width={900} height={480} alt="Picture of the author" />
           </div>
         </div>
 
-        <div className="descBoxCar">
-          <Image src="/car1.png" width={900} height={480} alt="Picture of the author" />
-        </div>
-      </div>
-
-      <div className="contactUs">
-        <div className="contactusBox">
+        <div className="contactUs">
           <span>CHOOSE AUTOMOBILE</span>
-          <hr />
-          <p>Choose a car from our catalog or offer your own option</p>
-        </div>
-        <div className="contactusBox">
           <span>CONTACT TO US</span>
-          <hr />
-          <p>Contact us at any convenient time for you</p>
-        </div>
-        <div className="contactusBox">
           <span>TO CONCLUDE AGREEMENT</span>
-          <hr />
+
+          <div className="arrows">
+            <div className="contactUsArrow">
+              <div className="longArrow">
+                <MdOutlineArrowForwardIos fill="#BF3535" />
+              </div>
+              <div className="longArrow">
+                <MdOutlineArrowForwardIos fill="#BF3535" />
+              </div>
+              <div className="longArrow">
+                <MdOutlineArrowForwardIos fill="#BF3535" />
+              </div>
+            </div>
+          </div>
+
+          <p>Choose a car from our catalog or offer your own option</p>
+          <p>Contact us at any convenient time for you</p>
           <p>The contract is concluded within one day</p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
