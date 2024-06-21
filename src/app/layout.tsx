aa0ce0c104ce4c3651f8a49da034736926a420cf
Shahzod <sibragimov999@gmail.com>
2024-06-21 19:09:23 +0500

    font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.scss";
 import Navigation from "@/components/navigation/Navigation";
 import StoreProvider from "./StoreProvider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Auto Lider",
@@ -14,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <StoreProvider>
           <header>
             <Navigation />
